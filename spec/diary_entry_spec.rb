89b348b21f5caa9b83c1090e9fdb49a06184a1b8
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-20 16:47:08 +0000

    count words

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -6,4 +6,15 @@ describe DiaryEntry do
     expect(diary_entry.contents).to eq "my_contents"
   end
 
+  describe '#count_word' do
+    it 'returns 0 if no words' do
+      diary_entry = DiaryEntry.new("my_title", "")
+      expect(diary_entry.count_word).to eq 0
+    end
+
+    
+  end
+
+
+
 end
