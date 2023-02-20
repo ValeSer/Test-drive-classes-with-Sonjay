241a7d8ff86f9cb45386a9e02ec089bc8689194d
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-20 16:58:03 +0000

    count word code and test

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,14 +1,19 @@
 class DiaryEntry
-  def initialize(title, contents)
-    @title = title
-    @contents = contents
+    def initialize(title, contents)
+      @title = title
+      @contents = contents
+    end
+  
+    def title
+      return @title
+    end
+  
+    def contents
+      return @contents
+    end
+  
+    def count_word
+      return 0
+    end
+  
   end
-
-  def title
-    return @title
-  end
-
-  def contents
-    return @contents
-  end
-
