c2e817b79cf475e5205ea985b7026be3e08a286b
Valeria Serci <balerty84@gmail.com>
2023-02-20 16:03:47 +0000

    add test for contents

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,10 +1,15 @@
 class DiaryEntry
-  def initialize(title)
+  def initialize(title, contents)
     @title = title
+    @contents = contents
   end
 
   def title
     return @title
+  end
+
+  def contents
+    return @contents
   end
 
 
